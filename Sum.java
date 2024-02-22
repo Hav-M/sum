8ef4250f9d481104ec419ac1fc001b6dc06a7966
Havilah Moges <160839691+Hav-M@users.noreply.github.com>
2024-02-22 12:21:14 +0300

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Sum
+{
+public static void main(String args[])
+{
+Scanner input= new Scanner(System.in)
+
+int number;
+String operation; 
+
+int num1;
+int num2;
+
+int result=num1+num2;
+
+System.out.printf("Enter two numbers");
+num1=input.nextInt();
+num2=input.nextInt();
+System.out.println("Enter the opertator(+,-,*,/):" +operator);
+operation=input.next().charAt(0);
+System.out.println("The final result:" num1+num2); 
+}
+}
